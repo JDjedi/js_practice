37b870f7c1e61aa45fc7883da05b39107f5772e9
JDjedi <jcdiaz1201@gmail.com>
2017-04-07 21:54:31 -0500

    amended practice.js

--- practice.js
+++ practice.js
@@ -8,17 +8,31 @@ function PeopleConstructor() {
 var populateArray = function(PeopleConstructor){
   var personArray = [];
   
-  while (personArray.length < 100){
+  while (personArray.length < 10){
     var person = new PeopleConstructor();
     person.sex = "M";
     person.politicalAffiliation = Math.random();
     personArray.push(person);
   }
   
-  sortPopulation(personArray);
+  //sortPopulationByIdeology(personArray);
+  politicalEventOne(personArray);
+  //sortPopulation(personArray);
 }
 
-var sortPopulation = function(personArray){
+var populationOverloadChecker = function(person){
+  if (person.politicalAffiliation > 1) {
+    person.politicalAffiliation = 1;
+    return person;
+  } else if (person.politicalAffiliation < 0) {
+    person.politicalAffiliation = 0;
+    return person;
+  } else {
+    return person;
+  }
+}
+
+var sortPopulationByIdeology = function(personArray){
   var right = [];
   var left = [];
   
@@ -30,10 +44,42 @@ var sortPopulation = function(personArray){
     }
   });
   
+  console.log("The Right");
   console.log(right);
-  console.log("");
+  console.log("The Left");
   console.log(left);
+  console.log("");
 }
 
+var politicalEventOne = function(personArray){
+  personArray.forEach(function(element){
+    if (element.politicalAffiliation >= .5) {
+      element.politicalAffiliation = element.politicalAffiliation + .1;
+      populationOverloadChecker(element);
+    } else {
+      element.politicalAffiliation = element.politicalAffiliation - .1;
+      populationOverloadChecker(element);
+    }
+  });
+
+  sortPopulation(personArray);
+}
+
+var sortPopulation = function(personArray){
+  personArray.sort(function(a, b) {
+    return parseFloat(b.politicalAffiliation) - parseFloat(a.politicalAffiliation);
+  });
+  console.log(personArray);
+}
+
+
 populateArray(PeopleConstructor);
+
+
+
+
+
+
+
+
 
