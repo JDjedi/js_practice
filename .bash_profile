1c6ef328c5ac25c3abd2f8d0563ff45332329ef5
JDjedi <jcdiaz1201@gmail.com>
2017-04-11 21:36:56 -0500

    Begun Node.js tutorials, also some work on practice.js

---
+++ .bash_profile
@@ -0,0 +1,9 @@
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+
+
+
+
+ctrl +o
+
+
--- practice.js
+++ practice.js
@@ -6,78 +6,60 @@ function PeopleConstructor() {
   this.polarization = false;
 }
 
-var populateArray = function(PeopleConstructor){
-  var personArray = [];
-  
-  while (personArray.length < 10){
-    var person = new PeopleConstructor();
-    person.sex = "M";
-    person.politicalAffiliation = Math.random();
-    personArray.push(person);
-  }
-  displayPopulation(personArray);
-  politicalEvent(personArray);
-}
-
-var populationPolarizationChecker = function(person){
-  if (person.politicalAffiliation > 1) {
-    person.politicalAffiliation = 1;
-    person.polarization = true;
-    return person;
-  } else if (person.politicalAffiliation < 0) {
-    person.politicalAffiliation = 0;
-    person.polarization = true;
-    return person;
-  } else {
-    return person;
-  }
-}
-
-var politicalEvent = function(personArray){
-  var digit = prompt("Please enter a digit: ");
-
-  personArray.forEach(function(element){
-    if (element.politicalAffiliation >= .5) {
-      element.politicalAffiliation = element.politicalAffiliation + parseFloat(digit);
-      populationPolarizationChecker(element);
-    } else {
-      element.politicalAffiliation = element.politicalAffiliation - parseFloat(digit);
-      populationPolarizationChecker(element);
+var objPolSim = {
+  populateArray: function(PeopleConstructor){
+    var personArray = [];
+    
+    while (personArray.length < 10){
+      var person = new PeopleConstructor();
+      person.sex = "M";
+      person.politicalAffiliation = Math.random();
+      personArray.push(person);
     }
-  });
-  displayPopulation(personArray);
-}
+    this.displayPopulation(personArray);
+    this.politicalEvent(personArray);
+  },
 
-//displays sorted population
-var displayPopulation = function(personArray){
-  personArray.sort(function(a, b) {
-    return parseFloat(b.politicalAffiliation) - parseFloat(a.politicalAffiliation);
-  });
-  console.log(personArray);
-  console.log("");
-}
-
-var displayPopulationByIdeology = function(personArray){
-  var right = [];
-  var left = [];
-  
-  personArray.forEach(function(element){
-    if (element.politicalAffiliation >= .5) {
-      right.push(element);
+  populationPolarizationChecker: function(person){
+    if (person.politicalAffiliation > 1) {
+      person.politicalAffiliation = 1;
+      person.polarization = true;
+      return person;
+    } else if (person.politicalAffiliation < 0) {
+      person.politicalAffiliation = 0;
+      person.polarization = true;
+      return person;
     } else {
-      left.push(element);
+      return person;
     }
-  });
-  
-  console.log("The Right");
-  console.log(right);
-  console.log("The Left");
-  console.log(left);
-  console.log("");
+  },
+
+  politicalEvent: function(personArray){
+    var digit = prompt("Please enter a digit: ");
+
+    personArray.forEach(function(element){
+      if (element.politicalAffiliation >= .5) {
+        element.politicalAffiliation = element.politicalAffiliation + parseFloat(digit);
+        objPolSim.populationPolarizationChecker(element);
+      } else {
+        element.politicalAffiliation = element.politicalAffiliation - parseFloat(digit);
+        objPolSim.populationPolarizationChecker(element);
+      }
+    });
+    this.displayPopulation(personArray);
+  },
+
+  //displays sorted population
+  displayPopulation: function(personArray){
+    personArray.sort(function(a, b) {
+      return parseFloat(b.politicalAffiliation) - parseFloat(a.politicalAffiliation);
+    });
+    console.log(personArray);
+    console.log("");
+  }
 }
 
-populateArray(PeopleConstructor);
-
+objPolSim.populateArray(PeopleConstructor);
 
 
 
