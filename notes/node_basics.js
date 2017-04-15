c779d5e20126834358bb9a98a4be3f4a4905cd46
JDjedi <jcdiaz1201@gmail.com>
2017-04-14 20:55:23 -0500

    completed notes for node.js

--- notes/node_basics.js
+++ notes/node_basics.js
@@ -1,62 +1,64 @@
 
 
 // Basic Promise 
-// var p = Promise.resolve('Hey I think this is working');
-//
-// p.then(function(message) {
-//   console.log(message);
-//   return message;
-//   })
-//     .then(function() {
-//       console.log('Second message!');
-//     })
-//       .then(function() {
-//         console.log('Third message!')
-// });
+var p = Promise.resolve('Hey I think this is working');
+
+p.then(function(message) {
+  console.log(message);
+  return message;
+  })
+    .then(function() {
+      console.log('Second message!');
+    })
+      .then(function() {
+        console.log('Third message!')
+});
+
+
 
 // Promise control flow
-// var testEven = new Promise(function(resolve, reject) {
-// 	if (Date.now() % 2 === 0) {
-// 		resolve('Even');
-// 	} else {
-// 		reject('Odd');
-// 	}
-// });
+var testEven = new Promise(function(resolve, reject) {
+	if (Date.now() % 2 === 0) {
+		resolve('Even');
+	} else {
+		reject('Odd');
+	}
+});
 
-// testEven.then(function(result) {
-// 		console.log(result);
-// })
-// 	.catch(function(err) {
-// 		console.error(err);
-// });
+testEven.then(function(result) {
+		console.log(result);
+})
+	.catch(function(err) {
+		console.error(err);
+});
+
 
 
 // Resolve multiple promises with promise.all
+var nums = [1, 2, 3, 4, 5];
+nums = nums.map(function(i) {
+	return Promise.resolve(i + 2);
+});
 
-// var nums = [1, 2, 3, 4, 5];
-// nums = nums.map(function(i) {
-// 	return Promise.resolve(i + 2);
-// });
-
-// Promise.all(nums)
-// 	.then(function(results) {
-// 		console.log(results);
-// 	})
-// 	.then(function() {
-// 		console.log('process is complete')
-// 	});
+Promise.all(nums)
+	.then(function(results) {
+		console.log(results);
+	})
+	.then(function() {
+		console.log('process is complete')
+	});
 
 
 
 // Async callback
-// var fs = require('fs');
-// var path = './lorem.txt';
+var fs = require('fs');
+var path = './lorem.txt';
 
-// fs.readFile(path, 'utf8', function(err, data) {
-//   err ? console.error(err) : console.log(data); //checks for erro first and if none given, proceeds
-// });
+fs.readFile(path, 'utf8', function(err, data) {
+  err ? console.error(err) : console.log(data); //checks for erro first and if none given, proceeds
+});
 
-// console.log('Done!') //gets to here and then return call back function above
+console.log('Done!') //gets to here and then return call back function above
 
 
 
