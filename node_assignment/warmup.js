ce32482ed5f39bd642e094bda15948eb7b983faf
JDjedi <jcdiaz1201@gmail.com>
2017-04-16 20:36:38 -0500

    updated warmup.js

--- node_assignment/warmup.js
+++ node_assignment/warmup.js
@@ -6,21 +6,71 @@
 // 	});
 // }, timer);
 
-var delay = function(timer){
-	return new Promise(function(resolve) {
-		setTimeout(function() {console.log(timer);}, timer);
-		resolve();
-	});
+// var delay = function(timer){
+// 	return new Promise(function(resolve) {
+// 		setTimeout(function() {resolve(timer);}, timer);
+// 	});
+// }
+
+// var countdown = function(result) {
+// 	console.log(result);
+// 	return delay(result - 100);
+// }
+
+// delay(3000)
+// 	.then(countdown)
+// 	.then(countdown)
+// 	.then(countdown)
+// 	.then(countdown);
+
+// var numbersToBeMapped = [1,2,3,4,5,7,8];
+// var squared = function(number) {
+// 	return new Promise(function(resolve, reject) {
+// 		if (isNaN(number)) {
+// 			reject('The number you entered is not a number; NaN error');
+// 		} else {
+// 			resolve(Math.pow(number, 2));
+// 		}
+// 	});
+// }
+
+// numbersSquared = numbersToBeMapped.map(function(i) {
+// 	return Promise.resolve(squared(i))
+// })
+
+// Promise.all(numbersSquared)
+// 	.then(function(results) {
+// 		console.log(results);
+// 	})
+// 	.then(function() {
+// 		console.log('The process is complete!')
+// 	})
+// ;
+
+var doBadThing = function(forRealz) {
+	return new promise(function(resolve, reject) {
+		if (forRealz != true) {
+			resolve('Yay!');
+		}
+	})
 }
 
-var countdown = function() {
-	return Promise.resolve(timer - 100);
-}
+doBadThing(NaN);
 
 
 
-delay(3000)
-	.then(countdown)
-	.then(countdown)
-	.then(countdown)
-	.then(countdown);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
