f6ad7ac534cb4f72b63440022bfb0959e6cc00db
JDjedi <jcdiaz1201@gmail.com>
2017-04-25 19:55:58 -0500

    Express hello completed

--- express_hello/index.js
+++ express_hello/index.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const app = express();
 
-// app.get('/', function (req, res) {
-// 	res.send('Hello Viking')
-// })
+app.set('view engine', 'hbs');
+
+app.use(express.static("public"))
+
+app.get('/', function (req, res) {
+	const name = req.params.name;
+	res.render('hello', {name: "Guest"});
+})
 
 app.get('/names/:name', function (req, res) {
-	const name = req.params.name;
-	res.send('Hello ' + name);
+  const name = req.params.name;
+  res.render('hello', { name: name })
+})
+
+app.use(function (req, res, next) { //middleware function
+  console.log(req.url + " — " + new Date())
+  next()
 })
 
 app.listen(3000, function() {
