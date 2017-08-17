da624c003e3e9649b8b735aebd61f8294a24d673
JDjedi <jcdiaz1201@gmail.com>
2017-08-17 10:50:25 -0500

    Coding challenge completed, substring_checker.js

--- practice/substring_checker.js
+++ practice/substring_checker.js
@@ -2,14 +2,18 @@
 "use strict";
 
 function solution(word, sub_word) {
-  if (word.includes(sub_word)){
-    
+  
+  var sub_word_test_length = sub_word.length;
+  var word_test = word.slice(sub_word_test_length * -1);
+
+  if (word_test === sub_word){
+    console.log('True');
   } else {
     console.log('False');
   }
 }
 
 solution('abc', 'bc');
-
-
+solution('kjnefkjr', 'kjw');
+solution('doggo', 'ggo');
 
