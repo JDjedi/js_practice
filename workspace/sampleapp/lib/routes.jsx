2a0fde17146fc07ea66dac726e6a7b930238685c
JDjedi <jcdiaz1201@gmail.com>
2017-10-25 21:16:04 -0500

    More work done on Sample App project for meteor-react

--- workspace/sampleapp/lib/routes.jsx
+++ workspace/sampleapp/lib/routes.jsx
@@ -1,15 +1,15 @@
 
 Router.route('/', function() {
-	this.layout('IndexLayout');
 	this.render('index');
+	//this.render('UserLoggedInLayout', { to: 'footer'});
 });
 
 Router.route('/about', function() {
-	this.layout('AboutLayout');
 	this.render('about');
+	//this.render('UserLoggedInLayout', { to: 'footer'});
 });
 
-// Router.configure({ //used to make a default template for all routes
-// 	layoutTemplate: 'ApplicationLayout'
-// });
+Router.configure({ //used to make a default template for all routes
+	layoutTemplate: 'ApplicationLayout'
+});
 
