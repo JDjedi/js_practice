517089bd7223a8d40cb348f884c2a0b4607aca03
JDjedi <jcdiaz1201@gmail.com>
2019-01-10 13:44:51 -0600

    Objects lessons updated

--- UdemyJs/objects/objects.js
+++ UdemyJs/objects/objects.js
@@ -88,18 +88,38 @@
 
 let restaurant = {
     name: 'Ramen Tatsuya',
-    guestCapacity: 65,
+    guestCapacity: 60,
     guestCount: 0,
 
-    checkAvailability: function(partySize) {
+    checkAvailability: function() {
         //console.log(this) // 'this' references the object the method is defined on
 
-        let seatsLeft = this.guestCapacity - this.guestCount
-        return partySize <= seatsLeft
+        if (this.guestCount <= this.guestCapacity) {
+            console.log(`You may seat more guests, there is room!`)
+            restaurant.seatParty(11)
+        } else {
+            console.log(`You may not seat additional guests, there is not enough room!`)
+            restaurant.removeParty(9)
+        }
+    },
+
+    seatParty: function(partySizeToAdd) {
+        this.guestCount = this.guestCount + partySizeToAdd
+        console.log(`You are seating ${this.guestCount} out of ${this.guestCapacity} available seats.`)
+    },
+
+    removeParty: function(partySizeToLeave){
+        this.guestCount = this.guestCount - partySizeToLeave
+        console.log(`You are seating ${this.guestCount} out of ${this.guestCapacity} available seats.`)
     }
 }
 
-let status = restaurant.checkAvailability(69)
+restaurant.checkAvailability()
+restaurant.checkAvailability()
+restaurant.checkAvailability()
+restaurant.checkAvailability()
+restaurant.checkAvailability()
+restaurant.checkAvailability()
+restaurant.checkAvailability()
 
-console.log(status)
 
